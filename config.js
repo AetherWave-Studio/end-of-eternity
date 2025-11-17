677ffff554572e335549758facc47b757d79595e
AetherWave-Studio <andrew.froehlich@aetherwavestudio.com>
2025-11-17 06:05:48 -0700

    added audio

--- config.js
+++ config.js
@@ -4,11 +4,11 @@ window.EOE_CONFIG = {
   PAYPAL_CLIENT_ID: 'BAAYXiJcYXG0sCzx282KYspXtHfq4ARVIetA9RN7rjgaTfJX85s4t51hwdqWh3UirggjeUB3Cvb9gb5E80',
   CONTACT_EMAIL: 'endofeternitofficial@gmail.com',
   PRODUCTS: [
-    { sku: 'ALBUM_YR0', title: 'Year Zero - CD', price: 10.00, type: 'physical', image: 'https://www.dropbox.com/scl/fi/yzid2sg6ijogbguxzdsgd/Year-Zero-cover-v01.png?rlkey=svl82f8db0ot0zbr7ydn7eokm&raw1' },
+    { sku: 'ALBUM_YR0', title: 'Year Zero - CD', price: 10.00, type: 'physical', image: '/assets/Year-Zero.png' },
     { sku: 'TEE_YR0', title: 'Year Zero - Tee', price: 30.00, type: 'physical', image: 'https://www.dropbox.com/scl/fi/z7l51rqkjjxomh65ju7lv/20250414_164901.jpg?rlkey=2r15136e9xifs99taxlmta24g&raw=1' },
     { sku: 'POSTER_COMIC', title: 'Comic Poster', price: 8.00, type: 'physical', image: 'https://www.dropbox.com/scl/fi/5aomso8mmusm6i0cfnnbd/Let_s-get-this-party-started-11x17-v01.png?rlkey=cgonf6pzdid1yvl49172fsoz0&raw=1' },
     { sku: 'STICKER', title: 'Sticker', price: 2.00, type: 'physical', image: '' },
     { sku: 'T_GHOST', title: 'Ghost', price: 1.00, type: 'digital', image: 'https://www.dropbox.com/scl/fi/mhi0tklpvpodddw0r6xuy/EagleChrome.png?rlkey=vvyrgh1qha5t3iujzaoav6o8y&raw=1', download: "/audio/Ghost GUNSHIP cover 89 bpm 01.wav" }
   ],
-  SPOTIFY: ['https://open.spotify.com/track/1qgLfV4yxkbPpNrBSZiPDH?si=de907e0022724fae']
+  SPOTIFY: ['https://open.spotify.com/track/1qgLfV4yxkbPpNrBSZiPDH?si=de907e0022724fae', 'https://open.spotify.com/track/3CgMy0NCBFLCkOKP02Ke5a?si=3742d0377f7a4896','https://open.spotify.com/artist/4fytwDqPsQRU72VocGC0c2?si=9e4128de8ac34ab3','https://open.spotify.com/track/4BfIO9R7dYA4LGGjgllaOc?si=72d72f096ec94f30','https://open.spotify.com/track/5q04iYYFlO6sQkuwYRThlI?si=70383bbf937b4217','https://open.spotify.com/track/2uOInYYhgWWzB8eB6CgHsR?si=729676cb469546c7','https://open.spotify.com/track/6rjsCedbWEAoQHJv2DHryI?si=115028c82cff46dd']
 };
