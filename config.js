51235c157bedef48c0287996fb11d7de56e369ce
AetherWave-Studio <andrew.froehlich@aetherwavestudio.com>
2025-11-17 05:09:07 -0700

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ window.EOE_CONFIG = {
     { sku: 'TEE_YR0', title: 'Year Zero - Tee', price: 30.00, type: 'physical', image: 'https://www.dropbox.com/scl/fi/z7l51rqkjjxomh65ju7lv/20250414_164901.jpg?rlkey=2r15136e9xifs99taxlmta24g&raw=1' },
     { sku: 'POSTER_COMIC', title: 'Comic Poster', price: 8.00, type: 'physical', image: 'https://www.dropbox.com/scl/fi/5aomso8mmusm6i0cfnnbd/Let_s-get-this-party-started-11x17-v01.png?rlkey=cgonf6pzdid1yvl49172fsoz0&raw=1' },
     { sku: 'STICKER', title: 'Sticker', price: 2.00, type: 'physical', image: '' },
-    { sku: 'T_ALLNIGHT', title: 'All Night (Drums)', price: 1.00, type: 'digital', image: 'http://dl.dropboxusercontent.com/scl/fi/xjvkvin4ffgaww8t7sisf/All-Night-STUDIO-01-with-drums-4-4-110bpm.wav?rlkey=f1efh6tjfp84ziawrwm6gvemi&dl=1', download: 'http://dl.dropboxusercontent.com/scl/fi/xjvkvin4ffgaww8t7sisf/All-Night-STUDIO-01-with-drums-4-4-110bpm.wav?rlkey=f1efh6tjfp84ziawrwm6gvemi&dl=1' },
-    { sku: 'T_GHOST', title: 'Ghost', price: 1.00, type: 'digital', image: '', download: '\audio\Ghost GUNSHIP cover 89 bpm 01.wav' }
+    { sku: 'T_ALLNIGHT', title: 'All Night (Drums)', price: 1.00, type: 'digital', image: 
+    { sku: 'T_GHOST', title: 'Ghost', price: 1.00, type: 'digital', image: 'https://www.dropbox.com/scl/fi/mhi0tklpvpodddw0r6xuy/EagleChrome.png?rlkey=vvyrgh1qha5t3iujzaoav6o8y&raw=1', download: '\audio\Ghost GUNSHIP cover 89 bpm 01.wav' }
   ],
   SPOTIFY: ['https://open.spotify.com/track/1qgLfV4yxkbPpNrBSZiPDH?si=de907e0022724fae']
 };
